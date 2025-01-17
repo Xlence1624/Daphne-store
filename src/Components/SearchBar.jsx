1897ec7b49b4c19f37babe69171139b0838cec6f
Xlence1624 <86269930+Xlence1624@users.noreply.github.com>
2025-01-17 18:05:46 +0100

    Update SearchBar.jsx

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -9,7 +9,7 @@ const SearchBar = () => {
   const [visible, setVisible] = useState(false)
   useEffect(
     ()=>{
-if (location.pathname.includes('home')){
+if (location.pathname.includes('Home')){
 setVisible(true)
 }
 else{
